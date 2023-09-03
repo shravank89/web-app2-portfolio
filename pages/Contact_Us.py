18fba423cd30680e094fbdcf6f9393eb5f078191
Shravan <newshravank25@gmail.com>
2023-09-03 13:07:59 +0530

    final touch added

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -4,7 +4,7 @@ from send_email import send_email
 
 st.header("Contact Me!")
 
-with st.form(key='email_form'):
+with st.form(key='email_form', clear_on_submit=True):
     email = st.text_input("Your email address")
     message = st.text_area("Your message")
     button = st.form_submit_button("Submit")
